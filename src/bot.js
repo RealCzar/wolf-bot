a9e1134b511216284f62232d1bef6cfb832f147a
RealCzar <czarsuhito@gmail.com>
2018-05-11 10:25:09 -0700

    started bot.js & added procfile

---
+++ src/bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = ">"
+
+client.on('ready', () => {
+  console.log('Wolf bot is now ready!');
+});
+
+client.on('message', message => {
+  if(message.content === prefix + "hello") {
+    message.reply("Hello, " + message.author.tag + "!");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
